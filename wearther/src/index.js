68975a82e701dcbc4ac05185097cedf6544c1752
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-01-11 14:26:59 -0800

    Started on form

--- wearther/src/index.js
+++ wearther/src/index.js
@@ -3,13 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class App extends React.Component {
-    //...
+    constructor(props) {
+        super(props);
+        this.state = {
+            clothes: Array(),
+        }
+    }
     render() {
         return (
-            <h1>Hello!</h1>
+            <div className="app">
+                <h1>Hello!</h1>
+                <Form />
+            </div>
         );
     }
 }
+
+class Form extends React.Component {
+    render() {
+        return(
+            <div className="form">
+                <form>
+                    <fieldset>
+                        <label for="customName">Custom Name (Optional)</label>
+                        <input type="text" id="customName" name="customName" />
+                    </fieldset>
+                    <fieldset>
+                        <label for="garmentType">Type of Garment</label>
+                        <select id="garmentType" name="garmentType">
+                            <option value="tshirt">T Shirt</option>
+                            <option value="downjacket">Down Jacket</option>
+                            <option value="insulatedparka">Insulated Parka</option>
+                        </select>
+                    </fieldset>
+                    <button type="submit">Add</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+// --------------------
 
 ReactDOM.render(
     <App />,
