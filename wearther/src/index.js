cc45438e4ec0cd5f9db56760231d1036f770fdce
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-01-11 16:02:41 -0800

    Tried to separate into multiple files, but JS/Node/CRA importing not working

--- wearther/src/index.js
+++ wearther/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+const components = require('./components');
 
 class App extends React.Component {
     constructor(props) {
@@ -20,59 +21,13 @@ class App extends React.Component {
         return (
             <div className="app">
                 <h1>Hello!</h1>
-                <Form onSubmit={(customName, garmentType) => this.addGarment(customName, garmentType)}/>
+                <components.Form onSubmit={(customName, garmentType) => this.addGarment(customName, garmentType)}/>
             </div>
         );
     }
 }
 
-class Form extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            customNameValue: "",
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleChange(event) {
-        this.setState({
-            customNameValue: event.target.value,
-        });
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        this.props.onSubmit(event.target.customName.value, event.target.garmentType.value);
-    }
-    render() {
-        return(
-            <div className="form">
-                <form onSubmit={this.handleSubmit}>
-                    <fieldset>
-                        <label htmlFor="customName">Custom Name (Optional)</label>
-                        <input 
-                            type="text" 
-                            id="customName" 
-                            name="customName" 
-                            value={this.state.customNameValue} 
-                            onChange={this.handleChange}
-                        />
-                    </fieldset>
-                    <fieldset>
-                        <label htmlFor="garmentType">Type of Garment</label>
-                        <select id="garmentType" name="garmentType">
-                            <option value="tshirt">T Shirt</option>
-                            <option value="downjacket">Down Jacket</option>
-                            <option value="insulatedparka">Insulated Parka</option>
-                        </select>
-                    </fieldset>
-                    <button type="submit">Add</button>
-                    <button type="reset">Reset</button>
-                </form>
-            </div>
-        );
-    }
-}
+
 
 // --------------------
 
