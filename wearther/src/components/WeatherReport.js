2657d40a7057352836d0c0fd32daccf4099f0e1c
mingkaic1 <mingkaic@andrew.cmu.edu>
2022-01-14 10:57:49 -0800

    Location and Temperature shows blank if no weather data

--- wearther/src/components/WeatherReport.js
+++ wearther/src/components/WeatherReport.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class LocationValue extends React.Component {
+    render() {
+        if (this.props.latitude !== null && this.props.longitude !== null) {
+            return(
+                <span>
+                    {this.props.city} <span className="text-muted">({this.props.latitude}, {this.props.longitude})</span>
+                </span>
+            );
+        } else {
+            return(
+                <span>
+                    {this.props.city}
+                </span>
+            );
+        }
+    }
+}
+class TemperatureValue extends React.Component {
+    render() {
+        if (this.props.temp !== null && this.props.tempMin !== null && this.props.tempMax !== null) {
+            return(
+                <span>
+                    {this.props.temp} &deg;C <span className="text-muted">({this.props.tempMin} - {this.props.tempMax})</span>
+                </span>
+            );
+        } else if (this.props.temp !== null) {
+            return(
+                <span>
+                    {this.props.temp} &deg;C
+                </span>
+            );
+        } else {
+            return(
+                null
+            );
+        }
+    }
+}
+
 export class WeatherReport extends React.Component {
     constructor(props) {
         super(props);
@@ -18,14 +57,20 @@ export class WeatherReport extends React.Component {
                 </h2>
                 <ul className="list-unstyled">
                     <li>
-                        <b>Location: </b> 
-                        <span>{this.props.location.city} </span>
-                        <span className="text-muted">({this.props.location.latitude}, {this.props.location.longitude})</span>
+                        <b>Location: </b>
+                        <LocationValue 
+                            city={this.props.location.city} 
+                            latitude={this.props.location.latitude} 
+                            longitude={this.props.location.longitude} 
+                        />
                     </li>
                     <li>
                         <b>Temperature: </b>
-                        <span>{this.props.weather.temp} &deg;C </span>
-                        <span className="text-muted">({this.props.weather.tempMin} - {this.props.weather.tempMax})</span>
+                        <TemperatureValue
+                            temp={this.props.weather.temp}
+                            tempMin={this.props.weather.tempMin}
+                            tempMax={this.props.weather.tempMax}
+                        />
                     </li>
                     <li>
                         <b>Rain/Snow: </b>{this.props.weather.precipitationProb}% chance
